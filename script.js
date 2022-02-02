3f8cba301d37f8880b6c27ed6373950b1e75c284
Mike Nguyen <minhmnguyen@cmail.carleton.ca>
2022-02-01 20:24:45 -0500

    Roll dice functionality

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 'use strict';
+
+/*Element selector*/
+//Players score
+const player1Score = document.getElementById('score--0');
+const player2Score = document.getElementById('score--1');
+
+//Current score
+const currentScore1 = document.getElementById('current--0');
+const currentScore2 = document.getElementById('current--1');
+
+//Current player
+const player1 = document.querySelector('.player--0');
+const player2 = document.querySelector('.player--1');
+
+//Buttons
+const newGame = document.querySelector('.btn--new');
+const rollDice = document.querySelector('.btn--roll');
+const holdScore = document.querySelector('.btn--hold');
+
+//Dice
+const diceElement = document.querySelector('.dice');
+
+//Init
+let currentScore = 0;
+diceElement.classList.add('hidden');
+
+/*Roll dice functionality*/
+rollDice.addEventListener('click', function () {
+	const randomRoll = Math.trunc(Math.random() * 6) + 1;
+	diceElement.classList.remove('hidden');
+	diceElement.src = `dice-${randomRoll}.png`;
+
+	if (randomRoll != 1) {
+		currentScore += randomRoll;
+		currentScore1.textContent = currentScore;
+	} else {
+		currentScore = 0;
+		player1.classList.remove('player--active');
+		player2.classList.add('player--active');
+	}
+});
