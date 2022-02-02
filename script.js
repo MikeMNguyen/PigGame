f29f92be3d22685f0c45ab9c3b143120bf21deea
Mike Nguyen <minhmnguyen@cmail.carleton.ca>
2022-02-02 07:03:21 -0500

    Disable button if winner detected

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ const holdScore = document.querySelector('.btn--hold');
 const diceElement = document.querySelector('.dice');
 
 //Init
+let playing = true;
 let currentScore = 0;
 let activePlayer = 0;
 const scores = [0, 0];
@@ -29,44 +30,57 @@ diceElement.classList.add('hidden');
 
 /*Roll dice functionality*/
 rollDice.addEventListener('click', function () {
-	const randomRoll = Math.trunc(Math.random() * 6) + 1;
-	diceElement.classList.remove('hidden');
-	diceElement.src = `dice-${randomRoll}.png`;
+	if (playing) {
+		const randomRoll = Math.trunc(Math.random() * 6) + 1;
+		diceElement.classList.remove('hidden');
+		diceElement.src = `dice-${randomRoll}.png`;
 
-	if (randomRoll != 1) {
-		currentScore += randomRoll;
-		setScore(activePlayer, currentScore);
-	} else {
-		swithPlayer();
+		if (randomRoll != 1) {
+			currentScore += randomRoll;
+			setScore(activePlayer, currentScore);
+		} else {
+			swithPlayer();
+		}
 	}
 });
 
 /*Hold score functionality*/
 holdScore.addEventListener('click', function () {
-	scores[activePlayer] += currentScore;
-	document.getElementById(`score--${activePlayer}`).textContent =
-		scores[activePlayer];
+	if (playing) {
+		scores[activePlayer] += currentScore;
+		document.getElementById(`score--${activePlayer}`).textContent =
+			scores[activePlayer];
 
-	if (scores[activePlayer] >= 10) {
-		document
-			.querySelector(`.player--${activePlayer}`)
-			.classList.add('player--winner');
-		document
-			.querySelector(`.player--${activePlayer}`)
-			.classList.remove('player--active');
-	} else {
-		swithPlayer();
+		if (scores[activePlayer] >= 10) {
+			document
+				.querySelector(`.player--${activePlayer}`)
+				.classList.add('player--winner');
+			document
+				.querySelector(`.player--${activePlayer}`)
+				.classList.remove('player--active');
+			playing = false;
+		} else {
+			swithPlayer();
+		}
 	}
 });
 
+/**
+ * Set current score
+ * @param {*} active
+ * @param {*} currScore
+ */
 const setScore = function (active, currScore) {
 	document.getElementById(`current--${active}`).textContent = currScore;
 };
 
+/**
+ * Switch active player
+ */
 const swithPlayer = function () {
 	currentScore = 0;
 	setScore(activePlayer, currentScore);
-	activePlayer = activePlayer === 0 ? 1 : 0; //Switching active player
+	activePlayer = activePlayer === 0 ? 1 : 0;
 	player1.classList.toggle('player--active');
 	player2.classList.toggle('player--active');
 };
